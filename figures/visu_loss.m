dc9309d64ea42c697d42a3a54d2bba64886b2366
Rémi Flamary <remi.flamary@gmail.com>
2014-11-07 11:29:26 +0100

    loss figures 2

--- figures/visu_loss.m
+++ figures/visu_loss.m
@@ -1,6 +1,10 @@
 close all
 clear all
 
+% NOTE: the figures are generated with matlab, Octave might generet
+% differnt figures expetially concenening Latex text
+
+
 %% classification loss
 
 err=-2:0.01:2;
@@ -36,3 +40,35 @@ l=legend('Co\^ut 0-1','Hinge','Hinge$^2$','Logistique','Sigmoid')
 set(l,'Interpreter','Latex','Fontsize',fs);
 print('-depsc','visuloss_class.eps')
 
+
+%% regression loss
+
+err=-2:0.01:2;
+
+
+% class loss
+
+l2 = @(err) err.^2;
+l1 = @(err) abs(err);
+epsi = @(err) max(0,abs(err)-.5);
+
+
+
+lw=2;
+fs=18;
+
+figure(2)
+set(gcf,'defaulttextinterpreter','latex');
+clf
+hold on
+plot(err,l2(err),'b','LineWidth',lw)
+plot(err,l1(err),'g','LineWidth',lw)
+plot(err,epsi(err),'r','LineWidth',lw)
+hold off
+axis([-2 2 0 3])
+xlabel('$y-f(\mathbf{x})$','interpreter','Latex','FontSize',fs)
+ylabel('$L(y,f(\mathbf{x}))$','interpreter','Latex','FontSize',fs)
+title('Co\^uts de r\''egression','interpreter','Latex','FontSize',fs)
+l=legend('Co\^ut $\ell_2$ ','Co\^ut  $\ell_1$','$\epsilon$ insensible')
+set(l,'Interpreter','Latex','Fontsize',fs);
+print('-depsc','visuloss_reg.eps')
